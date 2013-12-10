e6e2092f13aee587b35079a9176e8f84f3e27cfb
Oliver Dressler <oliver.dressler@chem.ethz.ch>
2013-12-10 14:31:01 +0100

    Many changes: e.g. Environment, np.roll()

--- droplet.py
+++ droplet.py
@@ -1,51 +1,85 @@
 import numpy as np
 import math
+from pylab import scatter, axis, show, plot
 
 class Droplet(object):
 	def __init__(self):
-		self.center_point = (100, 100)
-		self.num_spikes = 3
+		self.center_point = (50, 25)
+		self.num_spikes = 50
 
-		self._spikes = np.array([0.1 for i in range(self.num_spikes)])
+		self._spikes = np.array([0.1 for i in xrange(self.num_spikes)])
 		self._angle = 360. / self.num_spikes
-		self._spike_indices = range(len(self._spikes))
-		self.mult_term = math.sin(self._angle) * 0.5 
+		self._spike_indices = xrange(self.num_spikes)
+		self._mult_term = math.sin(self._angle) * 0.5 
 
 	def get_diameter(self):
 		return 2 * sum(self._spikes)/self.num_spikes
 
 	def get_area(self):
-		mult = np.copy(self._spikes)
-		mult[[0, -1]] = mult[[-1, 0]]
-		area = np.sum(self._spikes * mult * self.mult_term)
-		return area
+		return np.sum(self._spikes * np.roll(self._spikes, 1) * self._mult_term)
 
-	def get_area_old(self):
+	def get_area_pure(self):
 		area = 0
 		for i, p in enumerate(self._spikes):
-			area += p * self._spikes[(i+1)%self.num_spikes] * self.mult_term
+			area += p * self._spikes[(i+1)%self.num_spikes] * self._mult_term
 		return area
 
 	def move_up_spikes(self, distance):
 		self._spikes += distance
 
 	def get_points(self):
-		points = []
+		x, y = [], []
 		for i, p in enumerate(self._spikes):
-			x = (p * math.cos(i*self._angle)) + self.center_point[0]
-			y = (p * math.sin(i*self._angle)) + self.center_point[1]
-			points.append((x, y))
-		return points
+			x.append((p * math.cos(i*self._angle)) + self.center_point[0])
+			y.append((p * math.sin(i*self._angle)) + self.center_point[1])
+		return x, y
 
 	def find_shape(self, area):
 		v = d.get_area()
+		n  = 0
 		while v < area:
-			d.move_up_spikes(0.1)
-			v = d.get_area_old()
+			n += 1
+			d.move_up_spikes(0.05)
+			v = d.get_area()
+		return n
+
+class Environment(object):
+	def __init__(self, lines):
+		self._lines = np.array(lines)
+
+	def closest_intersect(self):
+		for p1, p2 in 
+		x = (p1[0]*p2[1] - p1[1]*p2[0])*(p3[0] - p4[0]) - (p1[0] - p2[0])*(p3[0]*p4[1] - p3[1]*p4[0])
+		x /= ((p1[0] - p2[0])*(p3[1] - p4[1]) - (p1[1]-p2[1])*(p3[0]-p4[0]))
+
+		y = ((p1[0]*p2[1] - p1[1]*p2[0])*(p3[1] - p4[1])) - ((p1[1] - p2[1])*(p3[0]*p4[1] - p3[1]*p4[0]))
+		y /= ((p1[0] - p2[0])*(p3[1] - p4[1]) - (p1[1]-p2[1])*(p3[0]-p4[0]))
 
 
-d = Droplet()
-d.find_shape(10000000)
-#print d.get_area()
-#print d.get_area_old()
-# print d.get_points()
+
+if __name__ == '__main__':
+	# Setup environment
+	lines = [
+		[[10, 10], [100, 10]],
+		[[10, 50], [100, 50]],
+
+	]
+	
+	e = Environment(lines)
+
+	# Setup droplet
+	d = Droplet()
+	num = d.find_shape(100)
+	#print 'Needed {} runs'.format(num)
+	#assert d.get_area() == d.get_area_pure()
+
+	# Plot the environment
+	for p1, p2 in e._lines:
+		plot([p1[0], p2[0]], [p1[1], p2[1]], color='k', linestyle='-', linewidth=2)
+
+	# Plot the droplet
+	x, y = d.get_points()
+	scatter(x, y)
+
+	axis('equal')
+	show()
